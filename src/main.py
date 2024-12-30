91b83f1c86cbc135fa1451dcacf55db536175d0e
Cregiz <joni.tolonen5@gmail.com>
2024-12-30 18:24:21 +0200

    Cleaning things

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
-# src/main.py
-print("Hello World")
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+    print(node)
+
+if __name__ == "__main__":
+    main()
+
--- src/textnode.py
+++ src/textnode.py
@@ -7,4 +7,19 @@ class TextType(Enum):
     CODE_TEXT = "code"
     LINKS = "links"
     IMAGES = "images"
-    TESTI = "testi"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return False  
+    
+    def __repr__(self):
+        return f"TextNode({self.text!r}, {self.text_type!r}, {self.url!r})"
+    
+    
