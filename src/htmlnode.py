902c9e9d756df4ecf39c2345e576e604ad186ac6
Cregiz <joni.tolonen5@gmail.com>
2024-12-30 22:28:21 +0200

    Added leafnode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,5 +17,22 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode(tag='{self.tag}', value='{self.value}', children={len(self.children)}, props={self.props})"
+    
+    def LeafNode(HTMLNode):
+        def __init__(self, tag, value, props=None):
+            if value is None:
+                raise ValueError("All leaf nodes must have a value.")
+            
+            super().__init__(tag=tag, value=value, children=None, props=props)
+            self.children = []
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value.")
+        
+        if self.tag == None:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,7 @@ import unittest
 from htmlnode import HTMLNode
 
 
-class TestTextNode(unittest.TestCase):
+class TestHtmlNode(unittest.TestCase):
     def test_props_to_html_with_attributes(self):
         node = HTMLNode(tag="a", props={"href": "https://www.google.com", "target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
@@ -19,6 +19,24 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(len(parent_node.children), 1)
         self.assertEqual(parent_node.children[0].tag, "span")
         self.assertEqual(parent_node.children[0].value, "Hello")
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_with_tag_and_props(self):
+        node = LeafNode(tag="a", value="Click here", props={"href": "https://www.example.com", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<a href="https://www.example.com" target="_blank">Click here</a>')
+
+    def test_to_html_with_tag_no_props(self):
+        node = LeafNode(tag="p", value="This is a paragraph.")
+        self.assertEqual(node.to_html(), '<p>This is a paragraph.</p>')
+
+    def test_to_html_without_tag(self):
+        node = LeafNode(tag=None, value="Just some text.")
+        self.assertEqual(node.to_html(), "Just some text.")
+
+    def test_to_html_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode(tag="span", value=None)
+
 
 if __name__ == "__main__":
     unittest.main()
